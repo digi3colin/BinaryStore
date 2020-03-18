31a75076713312840b12cd635abc982ef984484e
Colin Leung <colin@digi3studio.com>
2020-03-18 17:43:58 +0800

    first commit.

---
+++ classes/BinaryStore.js
@@ -0,0 +1,174 @@
+/* store the quantity of inventory */
+/**
+ *
+ * @param {DataView} data
+ * @param {number} idx
+ * @param {number} size
+ * @return *
+ */
+const getByte = (data, idx, size) => {
+  const offsetBit = idx * size;
+  const offsetByte = Math.floor(offsetBit / 8);
+  const shiftBit   = offsetBit % 8;
+
+  const max = (2 ** size) - 1;
+
+  const frameSize   = (size <= 8) ? 8 : ((size <= 16) ? 16 : 32);
+  let frame;
+  switch (frameSize) {
+    case (8):
+      frame = data.getUint8(offsetByte);
+      break;
+    case (16):
+      frame = data.getUint16(offsetByte);
+      break;
+    default :
+      frame = data.getUint32(offsetByte);
+  }
+
+  const valueOffset = (frameSize - size - shiftBit);
+
+  return {offsetByte : offsetByte, data: data, mask: max, frame: frame, frameSize : frameSize, valueOffset: valueOffset};
+};
+
+/*
+
+version 1, data size 16
+00 1       1 00 00
+
+00 11 00 00 : 00 00 00 00 00 00 00 00 11 11 11 11 11 11 11 11
+<bytes[0]->   <bytes[0]-> <bytes[1]-> <bytes[2]-> <bytes[3]->
+<-header ->   <-    16bit frame    -> <-    16bit frame    ->
+
+version 1, data size 10
+00 1       0 10 10
+
+00 10 10 10 : 00 00 00 00 00 11 11 11 11 11 00 00 00 00 00 00
+<bytes[0]->   <bytes[0]-> <bytes[1]-> <bytes[2]-> <bytes[3]->
+<-header ->   <10bit frame-> <10bit frame-> <10bit frame->
+*/
+
+class BinaryStore{
+  /**
+   *
+   * @param {number} version
+   * @param {number} headerByteSize
+   * @param {number} dataBitSize
+   * @param {number[]}values
+   */
+  constructor(version= 1, headerByteSize= 1, dataBitSize= 8, values = []) {
+    if(version < 0 || headerByteSize < 0 || dataBitSize < 0 )throw new Error('BinaryStore: arguments cannot be negative');
+
+    this.version    = version;
+    this.headerByteSize = headerByteSize;
+    this.dataBitSize   = dataBitSize;
+    this.maxValue   = (2 ** dataBitSize) - 1;
+
+    //data bytes + 1 byte header
+    const bufferSize = this.headerByteSize + Math.ceil((Math.max(2, values.length) * this.dataBitSize) / 8);
+    this.setBuffer(new ArrayBuffer(bufferSize));
+
+    values.forEach((x, i) => this.write(i, x));
+
+    this.makeHeader(this.header, this.headerByteSize, this.version, this.dataBitSize);
+  }
+
+  /**
+   *
+   * @param {ArrayBuffer} buffer
+   */
+  setBuffer(buffer){
+    this.buffer = buffer;
+    this.header = new DataView(buffer, 0, this.headerByteSize);
+    this.data   = new DataView(buffer, this.headerByteSize);
+  }
+
+  /**
+   * @param {DataView} header
+   * @param {number} headerByteSize
+   * @param {number} version
+   * @param {number} dataBitSize
+   */
+  makeHeader(header, headerByteSize, version, dataBitSize){
+    //create default header;
+    switch (headerByteSize) {
+      case (1):
+        header.setUint8(0, version << 5 | dataBitSize);
+        break;
+      case (2):
+        header.setUint16(0, version << 5 | dataBitSize);
+        break;
+      case (3):
+        header.setUint16(0, version);
+        header.setUint8(2, dataBitSize);
+        break;
+      case (4):
+        header.setUint32(0, version << 5 | dataBitSize);
+        break;
+      default :
+        header.setUint32(0, version);
+        header.setUint8(4, dataBitSize);
+    }
+  }
+
+  /**
+   * @param {number} idx
+   * @return number
+   */
+  read(idx){
+    const {mask, frame, valueOffset} = getByte(this.data, idx, this.dataBitSize);
+    return (frame >> valueOffset) & mask;
+  }
+
+  expandBuffer(){
+    const destView = new Uint8Array(new ArrayBuffer((this.buffer.byteLength - this.headerByteSize) * 2 + this.headerByteSize));
+    destView.set(new Uint8Array(this.buffer));
+    this.setBuffer(destView.buffer);
+  }
+
+  /**
+   * @param {number} idx
+   * @param {number} value
+   */
+  write(idx, value){
+    if(value > this.maxValue)throw new Error('value is bigger than ' + this.maxValue);
+    if(idx >= (this.buffer.byteLength - this.headerByteSize)){
+      this.expandBuffer();
+    }
+
+    const {data, offsetByte, mask, frameSize, frame, valueOffset} = getByte(this.data, idx, this.dataBitSize);
+
+    const newFrame = frame & ~(mask << valueOffset) | (value << valueOffset);
+
+    switch (frameSize) {
+      case (8):
+        data.setUint8(offsetByte, newFrame);
+        break;
+      case (16):
+        data.setUint16(offsetByte, newFrame);
+        break;
+      default :
+        data.setUint32(offsetByte, newFrame);
+    }
+  }
+
+  toString(){
+    const head = BinaryStore.toBinaryString(new Uint8Array(this.buffer.slice(0, this.headerByteSize)));
+    const tail = BinaryStore.toBinaryString(new Uint8Array(this.buffer, this.headerByteSize), this.dataBitSize);
+    return head + ": " + tail;
+  }
+
+  /**
+   * @param {Uint8Array} uint8Array
+   * @param {number} size
+   */
+  static toBinaryString(uint8Array, size = 8) {
+    return uint8Array
+      .reduce((a , x) => "" + a + x.toString(2).padStart( 8 , "0" ), "")
+      .replace(new RegExp(`(.{${size}})`, "g"),"$1 ") ;
+  }
+}
+
+Object.freeze(BinaryStore);
+Object.freeze(BinaryStore.prototype);
+module.exports = BinaryStore;
---
+++ tests/BinaryStore.test.js
@@ -0,0 +1,123 @@
+const BinaryStore = require('../classes/BinaryStore');
+
+describe('test binary store', () => {
+  test('1 byte header', () => {
+    const f = new BinaryStore(1, 1, 16);
+    expect(f.toString()).toBe('00110000 : 0000000000000000 0000000000000000 ');
+    expect(f.version).toBe(1);
+    expect(f.headerByteSize).toBe(1);
+    expect(f.maxValue).toBe(65535);
+  });
+
+  test('2 byte header', () => {
+    //header padding zero to higher bits if header length is not multiple of 8.
+    const version = 255;
+    const dataSize = 8;
+    const f = new BinaryStore(version, 2, dataSize, [255, 200]);
+    expect(version.toString(2)).toBe('11111111');
+    expect((version << 5).toString(2)).toBe('1111111100000');
+    expect((version << 5 | 8).toString(2)).toBe('1111111101000');
+
+    expect((255).toString(2)).toBe('11111111');
+    expect((200).toString(2)).toBe('11001000');
+
+    expect(f.toString()).toBe('00011111 11101000 : 11111111 11001000 ');
+  });
+
+  test('3 byte header', () => {
+    //header padding zero to higher bits if header length is not multiple of 8.
+    const f = new BinaryStore(255, 3, 8, [255, 200]);
+
+    expect(f.toString()).toBe('00000000 11111111 00001000 : 11111111 11001000 ');
+  });
+
+  test('4 byte header', () => {
+    //header padding zero to higher bits if header length is not multiple of 8.
+    const version = 255;
+    const dataSize = 8;
+    const f = new BinaryStore(version, 4, dataSize, [255, 200]);
+    expect(version.toString(2)).toBe('11111111');
+    expect((version << 5).toString(2)).toBe('1111111100000');
+    expect((version << 5 | 8).toString(2)).toBe('1111111101000');
+
+    expect((255).toString(2)).toBe('11111111');
+    expect((200).toString(2)).toBe('11001000');
+
+    expect(f.toString()).toBe('00000000 00000000 00011111 11101000 : 11111111 11001000 ');
+  });
+
+  test('init with values', () => {
+    const f = new BinaryStore(1563669060, 5, 16, [12000, 2413]);
+
+    /* 01011101 00110011 10110010 01000100 */
+
+    expect(f.toString()).toBe('01011101 00110011 10110010 01000100 00010000 : 0010111011100000 0000100101101101 ');
+
+    expect(f.read(0)).toBe(12000);
+    expect(f.read(1)).toBe(2413);
+  });
+
+  test('init with qty, 10 bit size', () => {
+    const f = new BinaryStore(1563669060, 5, 10, [520, 1023]);
+
+    expect(f.toString()).toBe('01011101 00110011 10110010 01000100 00001010 : 1000001000 1111111111 0000');
+
+    expect(f.read(0)).toBe(520);
+    expect(f.read(1)).toBe(1023);
+
+    f.write(3, 888);
+    expect(f.read(3)).toBe(888);
+    expect(f.toString()).toBe('01011101 00110011 10110010 01000100 00001010 : 1000001000 1111111111 0000000000 1101111000 00000000');
+
+    f.write(2, 56);
+    expect(f.read(3)).toBe(888);
+    expect(f.read(2)).toBe(56);
+    expect(f.toString()).toBe('01011101 00110011 10110010 01000100 00001010 : 1000001000 1111111111 0000111000 1101111000 00000000');
+
+    try{
+      f.read(4);
+      expect('').toBe('Error should be throw, this test should not be run.');
+    }catch(e){
+      expect(e.message).toBe('Offset is outside the bounds of the DataView');
+    }
+  });
+
+  test('assign overflow number', ()=>{
+    const f = new BinaryStore(1563669060, 5, 10, [520, 1023]);
+    try{
+      f.write(1, 1024);
+      expect('').toBe('Error should be throw, this test should not be run.');
+    }catch(e){
+      expect(e.message).toBe('value is bigger than 1023');
+    }
+  })
+
+  test('expand file', () => {
+    const f = new BinaryStore(1563669060, 5, 8, [120, 200, 80, 55]);
+    expect(f.toString()).toBe('01011101 00110011 10110010 01000100 00001000 : 01111000 11001000 01010000 00110111 ');
+    f.write(4, 33);
+    expect(f.toString()).toBe('01011101 00110011 10110010 01000100 00001000 : 01111000 11001000 01010000 00110111 00100001 00000000 00000000 00000000 ');
+  });
+
+  test('32bit integer', () => {
+    const f = new BinaryStore(1563669060, 5, 32, [2147483642]);
+    expect(f.toString()).toBe('01011101 00110011 10110010 01000100 00100000 : 01111111111111111111111111111010 00000000000000000000000000000000 ');
+    expect(f.read(0)).toBe(2147483642);
+
+  });
+
+  // is reading https://www.cs.odu.edu/~cs252/Book/branchcov.html
+  test('cover constructor', ()=>{
+    try{
+      const f = new BinaryStore(-1, -1, -16, [-100]);
+      expect('').toBe('Error should be throw, this test should not be run.');
+    }catch(e){
+      expect(e.message).toBe('BinaryStore: arguments cannot be negative');
+    }
+  });
+
+  test('cover constructor with default values', ()=>{
+    const f = new BinaryStore();
+    expect(f.toString()).toBe('00101000 : 00000000 00000000 ');
+  })
+});
